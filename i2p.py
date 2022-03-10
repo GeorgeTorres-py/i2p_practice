8166ccf3ed5816915d5fc4f1414030338bda36a0
George <70286427+GeorgeTorres-py@users.noreply.github.com>
2022-03-10 13:30:47 -0800

    Update i2p.py

--- i2p.py
+++ i2p.py
@@ -1,7 +1,6 @@
 #i2p dev practice from geti2p.net
 
 #destination and session creation
-
 dest = await i2plib.new_destination()
 print(dest.base32 + ".b32.i2p") #prints base32 addy
 
@@ -16,7 +15,6 @@ session_nickname = "test-i2p" #sessions must contain unique names
 session_writer = await i2plib.create_session(session_nickname, destination=dest)
 
 #making outgoing connections
-
 remote_host  = ""
 reader, writer =  await i2plib.stream_connect(session_nickname, remote_host)
 writer.write("GET / HTTP/1.0\nHost: {} \r\n\r\n".format(remote_host).encode())
@@ -31,3 +29,14 @@ while 1:
 
 writer.close()
 print(resp.decode())
+
+#accepting incoming connections
+
+async def handle_client(incoming, reader, writer):
+  dest, data = incoming.split(b"\n", 1)
+  remote_destination = i2plib.Destination(dest.decode())
+  if not data:
+      data = await reader.read(BUFFER_SIZE)
+  if data == b"PING":
+      writer.write(b"PONG")
+  write.close()
